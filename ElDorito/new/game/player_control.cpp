8c3d269c64a401b2de17435376a97c9a566a01a2
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-08-09 21:50:08 +0100

    `player_control_globals_get` bug fix

--- ElDorito/new/game/player_control.cpp
+++ ElDorito/new/game/player_control.cpp
@@ -8,7 +8,7 @@ namespace blam
     s_player_control_globals* player_control_globals_get()
     {
         Blam::Memory::s_thread_local_storage* tls = ElDorito::GetMainTls();
-        if (!tls || tls->player_control_globals)
+        if (!tls || !tls->player_control_globals)
             return nullptr;
 
         return tls->player_control_globals;
