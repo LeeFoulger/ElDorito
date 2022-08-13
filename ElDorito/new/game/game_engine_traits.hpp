703c05cf6c1e2dfa98812e6d633440d6df469ec3
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-08-13 09:09:20 +0100

    Update game_engine_variant.hpp

--- ElDorito/new/game/game_engine_traits.hpp
+++ ElDorito/new/game/game_engine_traits.hpp
@@ -4,26 +4,6 @@
 
 namespace blam
 {
-	enum e_game_engine_variant : long
-	{
-		_game_engine_base_variant = 0,
-		_game_engine_ctf_variant,
-		_game_engine_slayer_variant,
-		_game_engine_oddball_variant,
-		_game_engine_king_variant,
-		_game_engine_sandbox_variant,
-		_game_engine_vip_variant,
-		_game_engine_juggernaut_variant,
-		_game_engine_territories_variant,
-		_game_engine_assault_variant,
-		_game_engine_infection_variant,
-
-		k_game_engine_variants,
-		k_game_engine_variant_default = _game_engine_base_variant
-	};
-
-	extern const char* game_engine_variant_get_name(long index);
-
 	enum e_base_variant_flags
 	{
 		_base_variant_flags_built_in = 0,
--- ElDorito/new/game/game_engine_variant.hpp
+++ ElDorito/new/game/game_engine_variant.hpp
@@ -7,6 +7,24 @@
 
 namespace blam
 {
+	enum e_game_engine_variant : long
+	{
+		_game_engine_base_variant = 0,
+		_game_engine_ctf_variant,
+		_game_engine_slayer_variant,
+		_game_engine_oddball_variant,
+		_game_engine_king_variant,
+		_game_engine_sandbox_variant,
+		_game_engine_vip_variant,
+		_game_engine_juggernaut_variant,
+		_game_engine_territories_variant,
+		_game_engine_assault_variant,
+		_game_engine_infection_variant,
+
+		k_game_engine_variants,
+		k_game_engine_variant_default = _game_engine_base_variant
+	};
+
 	class c_bitstream;
 	struct c_game_engine_base_variant
 	{
