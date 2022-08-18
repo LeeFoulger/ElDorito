abd458623c6e80c0ca21588076277375e7684e59
LeeFoulger <71635168+LeeFoulger@users.noreply.github.com>
2022-08-18 03:08:45 +0100

    Stub cseries_string

---
+++ ElDorito/new/cseries/cseries_string.cpp
@@ -0,0 +1,14 @@
+#include "cseries_string.hpp"
+
+namespace blam
+{
+	//long csstricmp(charchar const* s1, char const* s1)
+	//long csstrnicmp(char const* s1, char const* s1, dword size)
+	//char* csstristr(char const* s1, char const* s1)
+	//char* csstrnzcpy(char* s1, char const* s2, dword size)
+	//char* csstrnzcat(char* s1, char const* s2, dword size)
+	//dword csstrnlen(char const* s, dword size)
+	//char* csstrnupr(char* s, dword size)
+	//char* csstrnlwr(char* s, dword size)
+	//char* csstrtok(char*, char const*, bool, struct csstrtok_data* data)
+}
